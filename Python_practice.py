2fb7d62df60e05cd4f02dbfd1bd4c792235b09fe
Rangisal <rangika.rowell@gmail.com>
2021-11-19 19:48:01 -0500

    Election_Analysis

---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+counties = ["ara", "den", "jeff"]
+if "elpa" in counties:
+  print("elpa is in the list of counties.")
+else:
+  print ("elpa is not the list of counties")
+
+for county in counties :
+  print (county)
+numbers = [1,2,3,4,]
+for num in numbers:
+  print (num)
+for county in counties_dict.keys():
+  print(county)
+  
